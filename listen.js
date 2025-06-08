d65b382abcde26ca7f9cb33cefd94b1b025b7dec
Bradley Mattison <bradleymattison2000@gmail.com>
2025-06-08 17:15:42 +0100

    final push before checking connection to production environment

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = 9090;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-  console.log("🚀 ~ app.listen ~ PORT:", PORT);
+  console.log("🚀 ~ Listening on ~ PORT:", PORT);
 });
